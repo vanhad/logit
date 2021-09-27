cba0eeca952dc8f59cc33975e4d5c825c7dce0a9
vanhad <vanhad72@gmail.com>
2021-09-27 20:28:08 +0530

    boto to get env

--- logitProj/settings.py
+++ logitProj/settings.py
@@ -15,7 +15,7 @@ import os
 
 import os
 import django_heroku
-from boto.s3.connection import S3Connection
+# from boto.s3.connection import S3Connection
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = S3Connection(os.environ['SECRET_KEY'])
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
